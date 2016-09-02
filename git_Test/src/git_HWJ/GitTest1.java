a0421921e5ed3014c7e698b3211854538b003fdb
nicolasjuhn <nicolasjuhn@hotmail.com>
2016-09-02 13:15:35 +0900

    주석추가

--- git_Test/src/git_HWJ/GitTest1.java
+++ git_Test/src/git_HWJ/GitTest1.java
@@ -6,6 +6,9 @@ public class GitTest1 {
 		// TODO Auto-generated method stub
 		System.out.println("hola vien glascias~! Guten tag~!");
 		System.out.println("안녕하세요~! 점심에 김치?~!");
+		//아헤!
+		//어휴 호랑이
+		//주석 커밋
 	}
 
 }
