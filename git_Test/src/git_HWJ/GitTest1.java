72926b694be5c02a135c3eb71c1a5662cb90f319
nicolasjuhn <nicolasjuhn@hotmail.com>
2016-09-02 11:46:43 +0900

    hola~! lunch~ proj 01

---
+++ git_Test/src/git_HWJ/GitTest1.java
@@ -0,0 +1,11 @@
+package git_HWJ;
+
+public class GitTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola vien glascias~! Guten tag~!");
+		System.out.println("안녕하세요~! 점심에 김치?~!");
+	}
+
+}
